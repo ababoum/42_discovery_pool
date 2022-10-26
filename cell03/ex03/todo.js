1c056026352678cb4fe7aae65f3bbb74c5f255ff
ababoum <ababou237@hotmail.com>
2022-10-26 12:06:42 +0200

    day 3 bis

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,13 +1,17 @@
 function new_task () {
     let res = prompt("My new task:");
 
-    if (res == "")
+    if (res == "" || res === null)
         return ;
 
     const newNode = document.createElement("div");
     const textNode = document.createTextNode("► " + res);
     newNode.appendChild(textNode);
     newNode.classList.add("task");
+	newNode.addEventListener('click', function delete_task(event) {
+		window.confirm()
+		newNode.remove();
+	});
 
 
     const parentDiv = document.getElementById("ft_list");
